93162e076e264cfc41e747c175d7160a4bff46cc
Dhanush123 <dhanush.patel@ymail.com>
2018-02-18 02:51:35 -0800

    ugh google docs...

--- css/main.js
+++ css/main.js
@@ -146,8 +146,15 @@ function startRecording() {
     }
 }
 
+
+var two_line = /\n\n/g;
+var one_line = /\n/g;
 function linebreak(s) {
   return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+}
+var first_char = /\S/;
+function capitalize(s) {
+  return s.replace(first_char, function(m) { return m.toUpperCase(); });
 }
 
 function stopRecording() {
