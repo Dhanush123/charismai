ffdb28b45c4a46c2fc925b3b91dd3a6b65df29f2
Dhanush123 <dhanush.patel@ymail.com>
2018-02-18 00:35:49 -0800

    r e s p o n s i v e

--- css/main.js
+++ css/main.js
@@ -165,43 +165,43 @@ function download() {
   }, 100);
 }
 
-(function() {
-var d3 = Plotly.d3;
+// (function() {
+// var d3 = Plotly.d3;
 
-var WIDTH_IN_PERCENT_OF_PARENT = document.getElementById("Plot-Container").width,
-    HEIGHT_IN_PERCENT_OF_PARENT = document.getElementById("Plot-Container").height;
+// var WIDTH_IN_PERCENT_OF_PARENT = document.getElementById("Plot-Container").width,
+//     HEIGHT_IN_PERCENT_OF_PARENT = document.getElementById("Plot-Container").height;
 
-var gd3 = d3.select('body')
-    .append('div')
-    .style({
-        width: WIDTH_IN_PERCENT_OF_PARENT + '%',
-        'margin-left': (100 - WIDTH_IN_PERCENT_OF_PARENT) / 2 + '%',
+// var gd3 = d3.select('body')
+//     .append('div')
+//     .style({
+//         width: WIDTH_IN_PERCENT_OF_PARENT + '%',
+//         'margin-left': (100 - WIDTH_IN_PERCENT_OF_PARENT) / 2 + '%',
 
-        height: HEIGHT_IN_PERCENT_OF_PARENT + 'vh',
-        'margin-top': (100 - HEIGHT_IN_PERCENT_OF_PARENT) / 2 + 'vh'
-    });
+//         height: HEIGHT_IN_PERCENT_OF_PARENT + 'vh',
+//         'margin-top': (100 - HEIGHT_IN_PERCENT_OF_PARENT) / 2 + 'vh'
+//     });
 
-var gd = gd3.node();
+// var gd = gd3.node();
 
-Plotly.plot(gd, [{
-  type: 'bar',
-  x: [1, 2, 3, 4],
-  y: [5, 10, 2, 8],
-  marker: {
-      color: '#C8A2C8',
-      line: {
-          width: 2.5
-      }
-  }
-}], {
-  title: 'Auto-Resize',
-  font: {
-      size: 16
-  }
-});
+// Plotly.plot(gd, [{
+//   type: 'bar',
+//   x: [1, 2, 3, 4],
+//   y: [5, 10, 2, 8],
+//   marker: {
+//       color: '#C8A2C8',
+//       line: {
+//           width: 2.5
+//       }
+//   }
+// }], {
+//   title: 'Auto-Resize',
+//   font: {
+//       size: 16
+//   }
+// });
 
-window.onresize = function() {
-  Plotly.Plots.resize(gd);
-};
+// window.onresize = function() {
+//   Plotly.Plots.resize(gd);
+// };
 
-})();
+// })();
